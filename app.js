41c386707a4f1b93d37115e91469f6ad7a44fa91
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-29 15:32:34 +0200

    code review

--- app.js
+++ app.js
@@ -9,51 +9,16 @@ const app = new Vue({
 
     },
 
-    methods: {},
+    methods: {
+
+    },
 
     mounted() {
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            //console.log(response.data)
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-        })
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-            this.emails.push(response.data.response)
-
-        })
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                this.emails.push(response.data.response)
+            })
+        }
 
     },
 
